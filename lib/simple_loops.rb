ad61acf328cdc39955a4808f996629be41c6bd31
erin dougher <ejdougher@gmail.com>
2020-11-23 06:20:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,12 +1,24 @@
 # Write your methods here
 
 def loop_message_five_times(string)
-  counter = 0
+  n = 0
   string = string
-  while counter < 5
+  while n < 5
     puts string
-    counter += 1
+    n += 1
   end
 end
 new_string = "Hello World"
+loop_message_five_times(new_string)
+
+def loop_message_n_times(string, integer)
+  n = 0
+  limit = integer
+  string = string
+  while n < limit
+    puts string
+    n += 1
+  end
+end
+new_string = "Hello Moon"
 loop_message_five_times(new_string)
