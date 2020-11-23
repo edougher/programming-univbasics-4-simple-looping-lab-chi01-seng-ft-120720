ae4f1c3d0aaf688bc0d9730e0c9f0d91ec9773c4
erin dougher <ejdougher@gmail.com>
2020-11-23 06:28:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,3 +22,14 @@ def loop_message_n_times(string, integer)
 end
 new_string = "Hello Moon"
 loop_message_five_times(new_string)
+
+def output_array(array)
+  n = 0
+  array = array
+  while array < array.length do
+    puts array[n]
+    n += 1
+  end
+  end
+  number_array = [5,4,3,2,1]
+  output_array(number_array)
