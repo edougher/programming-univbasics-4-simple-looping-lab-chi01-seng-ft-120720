543e2edfe9f4c324f2b7b0fc4df8584841222762
erin dougher <ejdougher@gmail.com>
2020-11-23 05:59:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,11 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  counter = 5
+  string = string
+  while counter < 6
+    puts string
+  end
+end
+new_string = "Hello World"
+loop_message_five_times(new_string)
